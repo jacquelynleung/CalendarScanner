8c60f16543e8f92a927830e56ba1d24c2c90d729
JacquelynLeung <jil4009@g.rit.edu>
2019-03-03 07:19:42 -0500

    Got tesseract to work with Google Calendar API!!!!

--- src/main/java/TesseractOCR.java
+++ src/main/java/TesseractOCR.java
@@ -10,8 +10,8 @@ public class TesseractOCR {
         Tesseract tesseract = new Tesseract();
         String fin = "";
         try {
-            tesseract.setDatapath("/Users/jleung00/Desktop/CalendarScanner/tessdata");
-            String text = tesseract.doOCR(new File("../resources/test3.png"));
+            tesseract.setDatapath("../resources/tessdata");
+            String text = tesseract.doOCR(new File("../resources/test4.png"));
             fin = text;
 
         } catch (TesseractException e) {
@@ -24,10 +24,12 @@ public class TesseractOCR {
         String[] array = new String[2];
         String date = "";
         String title = "";
-        for(int i = 0; text.charAt(i) != '\n' ;i++){
+        int l = text.length();
+        for(int i = 0; i < l ;i++){
             if(text.charAt(i) == '-'){
-                date = text.substring(i-1, i+6);
-                title = text.substring(0, i - 2);
+                date = text.substring(i-4, i+6);
+                title = text.substring(0, i - 5);
+                break;
             }
         }
         array[0] = title;
@@ -39,7 +41,19 @@ public class TesseractOCR {
     public static void main(String[] args) {
         String text = getText();
         System.out.println(text);
-        System.out.println(getInfo(text));
+        String lines[] = text.split("\\r?\\n");
+        String title = "";
+        String date = "";
+        for(String s : lines){
+            if(!s.equals("")){
+                String[] array = getInfo(s);
+                title = array[0];
+                date = array[1] + "T09:00:00-07:00";
+                System.out.println("Title: " + title + " Date: " + date);
+            }
+
+        }
+
     }
 
 }
