0cbed9ea7df0bc6f05a278a3fc4c29048c8bdffb
JacquelynLeung <jil4009@g.rit.edu>
2019-03-03 05:49:16 -0500

    Working on tesseract

--- src/main/java/TesseractOCR.java
+++ src/main/java/TesseractOCR.java
@@ -6,16 +6,34 @@ import net.sourceforge.tess4j.TesseractException;
 
 public class TesseractOCR {
 
-    public static void main(String []args) {
+    public static String getText() {
         Tesseract tesseract = new Tesseract();
+        String fin = "";
         try {
             tesseract.setDatapath("/Users/jleung00/Desktop/CalendarScanner/tessdata");
             String text = tesseract.doOCR(new File("/Users/jleung00/Desktop/test3.png"));
-            System.out.print(text);
+            fin = text;
 
         } catch (TesseractException e) {
             e.printStackTrace();
         }
+        return fin;
+    }
+
+    public static String[] getTitle(String text){
+        String lines[] = text.split("\\r?\\n");
+        return lines;
+    }
+
+    public static String getDate(String text){
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+        String text = getText();
+        System.out.println(text);
+        System.out.println(getDate(text));
     }
 
 }
